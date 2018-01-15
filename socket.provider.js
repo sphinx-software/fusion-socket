40b29962ea2296f6270981ca704152ab9aa85677
NgocTo <tongoc92@gmail.com>
2018-01-15 16:44:56 +0700

    use koa as middleware kernel for socket

--- socket.provider.js
+++ socket.provider.js
@@ -1,24 +1,39 @@
-const io = require('socket.io');
+const IO     = require('koa-socket');
+const Koa    = require('koa');
 const lodash = require('lodash');
 
 exports.register = async (container) => {
     container.singleton('socket.kernel', async () => {
-        const config     = await container.make('config');
-        return io(config.socket.options);
+        const config       = await container.make('config');
+        const socketKernel = new Koa(); 
+        const io           = new IO(config.socket.options);
+        
+        io.attach(socketKernel);    
+        return socketKernel;    
     });
 };
 
 exports.boot = async (container) => {
-    const config = await container.make('config');
+    const config       = await container.make('config');
     const socketKernel = await container.make('socket.kernel');
-    config.socket.middlewares.forEach((middleware) => socketKernel.use(middleware));
 
-    socketKernel.on('connection', (socket) => {
-        socket.container = container;
-        lodash.forEach(config.socket.handlers, (handlers, eventName) => {
-            handlers.forEach((handler) => {
-                socket.on(eventName, (data) => {handler(socket, data)});
-            })
+    // attach container
+    socketKernel.io.use(async (context, next) => {
+        context.container = container;
+        await next();
+    });
+    
+    // register middlewares
+    config.socket.middlewares.forEach((middleware) => socketKernel.io.use(middleware));
+
+    // register events    
+    lodash.forEach(config.socket.handlers, (handlers, eventName) => {
+        handlers.forEach((handler) => {
+            socketKernel.io.on(eventName, handler);
         })
     })
+
+    socketKernel.io.on('connection', (socket) => {
+        console.log(`client ${socket.socket.client.id} is connected`)
+    });
 };
